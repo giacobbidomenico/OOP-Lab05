8364d0a73b40829ddf4e640b639ae3fa41577334
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-08 17:39:00 +0100

    Update class UseSetWithOrder

--- src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -3,6 +3,9 @@ package it.unibo.oop.lab05.ex2;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * 
+ */
 public final class UseSetWithOrder {
 	private static final int NUM_STRINGS = 100;
 	
@@ -10,13 +13,19 @@ public final class UseSetWithOrder {
     
     }
     
+    /**
+     * @param s
+     *            ignored
+     */
     public static void main(final String[] args) {
     	/*1) Creates a new ORDERED TreeSet of Strings. To order the set, define
-             a new Comparator in a separate class.*/
+    	 *a new Comparator in a separate class.
+    	 */
     	Set<String> treeSet = new TreeSet<String>(new StringComparator());
     	
     	/* 2) Inserts in the set a hundred strings containing randomly generated
-              numbers (use Math.random())*/
+    	 * numbers (use Math.random())
+    	 */
     	for(int i = 0; i < NUM_STRINGS; i++) {
     		treeSet.add(Double.toString(Math.random()));
     	}
