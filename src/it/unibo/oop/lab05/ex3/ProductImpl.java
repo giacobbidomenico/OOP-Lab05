3bee4d16f4672c54400094b60819c96bc32d8cec
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-08 17:39:48 +0100

    Update class ProductImpl.java

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -1,9 +1,19 @@
 package it.unibo.oop.lab05.ex3;
 
+/**
+ * Product implementation.
+ * 
+ */
 public class ProductImpl implements Product{
 	private final String name;
 	private final double quantity;
 	
+	/**
+     * @param n
+     *            the product name
+     * @param q
+     *            the product quantity
+     */
 	public ProductImpl(final String name,final double quantity) {
 		this.name = name;
 		this.quantity = quantity;
@@ -23,5 +33,13 @@ public class ProductImpl implements Product{
 	
 	public String toString() {
 		return "("+ this.name +" , "+ this.quantity +")";
+	}
+	
+	public boolean equals(final Object object) {
+		if(object instanceof Product) {
+			final var tmp = (Product) object;
+			return this.name.equals(tmp.getName());
+		}
+		return false;
 	}
 }
