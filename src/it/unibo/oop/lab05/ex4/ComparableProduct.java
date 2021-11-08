646ddd3f12e5a6184ef705170c22d5c8db9e012f
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-08 17:41:26 +0100

    Update class ComparableProduct.java

--- src/it/unibo/oop/lab05/ex4/ComparableProduct.java
+++ src/it/unibo/oop/lab05/ex4/ComparableProduct.java
@@ -3,8 +3,17 @@ package it.unibo.oop.lab05.ex4;
 import it.unibo.oop.lab05.ex3.Product;
 import it.unibo.oop.lab05.ex3.ProductImpl;
 
+/**
+*
+*/
 public class ComparableProduct extends ProductImpl implements Comparable<Product>{
 	
+	/**
+     * @param name
+     *            the product name
+     * @param quantity
+     *            the product quantity
+     */
 	public ComparableProduct(final String name,final double quantity) {
 		super(name,quantity);		
 	}
