4810ae9161dfadf087e6b092fd93ce65e8a2496a
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-08 17:42:07 +0100

    Update class OrderedWarehouse.java

--- src/it/unibo/oop/lab05/ex4/OrderedWarehouse.java
+++ src/it/unibo/oop/lab05/ex4/OrderedWarehouse.java
@@ -6,6 +6,9 @@ import java.util.TreeSet;
 import it.unibo.oop.lab05.ex3.Product;
 import it.unibo.oop.lab05.ex3.WarehouseImpl;
 
+/**
+*
+*/
 public class OrderedWarehouse extends WarehouseImpl{	
 	
 	public Set<Product> allProduct() {
